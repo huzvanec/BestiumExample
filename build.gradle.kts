43764967d15836b9fd40e2f502d787dcd57073cc
Hužva <huzvanec@email.cz>
2025-04-26 18:03:13 +0200

    tada: initial commit

---
+++ build.gradle.kts
@@ -0,0 +1,52 @@
+plugins {
+    kotlin("jvm") version "2.1.20"
+    id("com.gradleup.shadow") version "8.3.0"
+    id("xyz.jpenilla.run-paper") version "2.3.1"
+}
+
+group = "cz.jeme"
+version = "1.0.0"
+
+repositories {
+    mavenCentral()
+    maven("https://repo.papermc.io/repository/maven-public/") {
+        name = "papermc-repo"
+    }
+    maven("https://oss.sonatype.org/content/groups/public/") {
+        name = "sonatype"
+    }
+}
+
+dependencies {
+    compileOnly("io.papermc.paper:paper-api:1.21.4-R0.1-SNAPSHOT")
+    implementation(kotlin("stdlib-jdk8"))
+}
+
+tasks {
+    runServer {
+        downloadPlugins{
+            modrinth("bettermodel", "1.5.1")
+        }
+        minecraftVersion("1.21.4")
+    }
+}
+
+val targetJavaVersion = 21
+kotlin {
+    jvmToolchain(targetJavaVersion)
+}
+
+tasks{
+    assemble {
+        dependsOn("shadowJar")
+    }
+}
+
+tasks.processResources {
+    val props = mapOf("version" to version)
+    inputs.properties(props)
+    filteringCharset = "UTF-8"
+    filesMatching("plugin.yml") {
+        expand(props)
+    }
+}
---
+++ src/main/kotlin/cz/jeme/bestiumexample/BestiumExample.kt
@@ -0,0 +1,14 @@
+package cz.jeme.bestiumexample
+
+import org.bukkit.plugin.java.JavaPlugin
+
+class BestiumExample : JavaPlugin() {
+
+    override fun onEnable() {
+        // Plugin startup logic
+    }
+
+    override fun onDisable() {
+        // Plugin shutdown logic
+    }
+}
